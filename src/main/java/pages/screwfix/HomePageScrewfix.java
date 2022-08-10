fa9681de184c01d7a8f49eb742e42534db9c2c77
Kateryna Koliieva <Kateryna_Koliieva@epam.com>
2022-08-10 18:51:49 +0300

    removed unnecessary functions

--- src/main/java/pages/screwfix/HomePageScrewfix.java
+++ src/main/java/pages/screwfix/HomePageScrewfix.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.BasePage;
-
-import static utils.Wait.waitForPageLoadComplete;
 import static utils.Wait.waitVisibilityOfElement;
 
 public class HomePageScrewfix extends BasePage {
@@ -14,26 +12,8 @@ public class HomePageScrewfix extends BasePage {
         super(driver);
     }
 
-    @FindBy(xpath = "//a[@id='header_my_account_link']")
-    private WebElement yourAccountButton;
-
-    @FindBy(xpath = "//a[@id='header_link_sign_out']")
-    private WebElement logOutButton;
-
     @FindBy(xpath = "//span[@class='user-name']")
     private WebElement signInButton;
-
-    public HomePageScrewfix clickTheYourAccountButton(){
-        waitForPageLoadComplete();
-        yourAccountButton.click();
-        return this;
-    }
-
-    public HomePageScrewfix clickTheLogOutButton(){
-        waitVisibilityOfElement(logOutButton);
-        logOutButton.click();
-        return this;
-    }
 
     public String getTextFromSignInButton() {
         waitVisibilityOfElement(signInButton);
