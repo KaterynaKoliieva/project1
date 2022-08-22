1359f521e411094ac11e715ca01ed024b5423358
Kateryna Koliieva <Kateryna_Koliieva@epam.com>
2022-08-23 00:16:41 +0300

    changed RunnerTests

--- src/test/java/runner/RunnerTests.java
+++ src/test/java/runner/RunnerTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/main/resources/authentication.feature",
+        features = {"src/main/resources/authentication.feature","src/main/resources/registration.feature",},
         glue = "stepdefinitions",
         publish = true
 )
